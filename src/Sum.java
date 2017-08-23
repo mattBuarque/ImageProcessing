a28b4848bf6d95cd3fbe5c93f78028b4fc28f7d5
mattBuarque <matheusluiz703@hotmail.com>
2017-08-23 02:33:50 -0300

    The Adding process (Sum of the pixel values) was added to the project

--- src/Sum.java
+++ src/Sum.java
@@ -3,8 +3,16 @@ import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
 
+import br.com.matheus.image.ImageLog;
 
 public class Sum {
+	
+	private static final String ORIGINAL_IMAGE = "Sum\\OriginalImage.gif";
+	private static final String MASKED_IMAGE = "Sum\\MaskedImage.gif";
+	private static final String EDGE_IMAGE = "Sum\\EdgeImage.gif";
+	private static final String MASK = "Sum\\Mask.gif";
+	
+	
 	public static int[][] sum(int[][] firstImage, int[][] secondImage){
 		int largura = firstImage.length;
 		int altura = firstImage[0].length;
@@ -13,9 +21,21 @@ public class Sum {
 		for (int y = 0; y < altura; y++) {
 			for (int x = 0; x < largura; x++) {
 				output[x][y] = (firstImage[x][y] + secondImage[x][y]);
-//				ImageLog.writeInLog("Primeira Imagem: " + firstImage[x][y]);
-//				ImageLog.writeInLog("Segunda Imagem: "  + secondImage[x][y]);
-//				ImageLog.writeInLog("Terceira Imagem: "  + output[x][y]);
+				if (secondImage[x][y] >= 10777215) {
+					int r = (secondImage[x][y] & 0x00ff0000 >> 16);
+					int g = (secondImage[x][y] & 0x0000ff00 >> 8);
+					int b = (secondImage[x][y] & 0x000000ff);
+					int r1 = (firstImage[x][y] & 0x00ff0000 >> 16);
+					int g1 = (firstImage[x][y] & 0x0000ff00 >> 8);
+					int b1 = (firstImage[x][y] & 0x000000ff);
+					int r2 = (output[x][y] & 0x00ff0000 >> 16);
+					int g2 = (output[x][y] & 0x0000ff00 >> 8);
+					int b2 = (output[x][y] & 0x000000ff);
+					
+					ImageLog.writeInLog("Primeira Imagem: " + r1 +" "+ g1 +" "+ b1);
+					ImageLog.writeInLog("Segunda Imagem: "  + r +" "+ g +" "+ b);
+					ImageLog.writeInLog("Terceira Imagem: " + r2 +" "+ g2 +" "+ b2);
+				}
 			}
 		}
 
@@ -23,8 +43,8 @@ public class Sum {
 	}
 
 	public static void main(String[] args) throws IOException {
-		BufferedImage img1 = ImageIO.read(new File("OriginalImage.gif"));
-		BufferedImage img2 = ImageIO.read(new File("EdgeImage.gif"));
+		BufferedImage img1 = ImageIO.read(new File(MASKED_IMAGE));
+		BufferedImage img2 = ImageIO.read(new File(EDGE_IMAGE));
 
 		ImageFormats.seeImage(img1);
 		ImageFormats.seeImage(img2);
